b2e8156fea38e56b42924c29e5e03510fb84d96a
Thomas Moerbauer <tmoer93@gmail.com>
2023-06-04 22:28:56 +0200

    :sparkles: Reworked branchlable to not be part of the directed graph

--- frontend/src/app/visualization/active-conflict-awareness/active-conflict-awareness.component.ts
+++ frontend/src/app/visualization/active-conflict-awareness/active-conflict-awareness.component.ts
@@ -9,6 +9,7 @@ import {
 import * as d3 from 'd3';
 import { forkJoin } from 'rxjs';
 import { graphlib, render } from 'dagre-d3-es';
+import { Graph } from 'dagre-d3-es/src/graphlib';
 
 @Component({
   selector: 'app-active-conflict-awareness',
@@ -59,15 +60,15 @@ export class ActiveConflictAwarenessComponent implements OnInit {
         this.commitsRelationships = v?.relationships?.content || [];
         this.nodes = v?.commits?.content;
         v?.commits?.content?.forEach(c => {
-          this.commits.set(c.id!!, c)
+          this.commits.set(c.id!, c)
         })
         this.links = v?.relationships?.content;
         v?.branches.content?.forEach(b => {
-          this.branchById.set(b.id!!, b);
-          if (this.branchesByHeadCommitId.has(b.headCommitId!!)) {
-            this.branchesByHeadCommitId.get(b.headCommitId!!)?.push(b);
+          this.branchById.set(b.id!, b);
+          if (this.branchesByHeadCommitId.has(b.headCommitId!)) {
+            this.branchesByHeadCommitId.get(b.headCommitId!)?.push(b);
           } else {
-            this.branchesByHeadCommitId.set(b.headCommitId!!, [b]);
+            this.branchesByHeadCommitId.set(b.headCommitId!, [b]);
           }
         });
         this.render()
@@ -82,70 +83,120 @@ export class ActiveConflictAwarenessComponent implements OnInit {
     const graph = new graphlib.Graph({directed: true});
     graph.setGraph({
       rankdir: 'RL',
-      ranksep: 30,
+      ranksep: 40,
       nodesep: 5
     });
     graph.setDefaultEdgeLabel(() => ({}));
 
     const svg = d3.select('#git-graph')
-    const g = svg.append('g');
-    const renderer: any = render();
+            .attr('width', '100%')
+            .attr('height', '90vh')
+            .call(d3.zoom<any, any>()
+                    .scaleExtent([0.1, 8])
+                    .on('zoom', function ({transform}) {
+                      g.attr('transform', transform);
+                    }))
+    const g = svg.append('g').attr('id', 'git-graph-group');
 
+    this.drawBaseGraph(graph, g);
+    this.setBranchLabel();
+
+    g.selectAll('g.node').on('mouseover', (event, commitNodeId) => {
+      if ((commitNodeId as string).indexOf(this.COMMIT_NODE_ID_PREFIX) === 0) {
+        const commitId = (commitNodeId as string).replace(this.COMMIT_NODE_ID_PREFIX, '');
+        const commit = this.commits.get(+commitId);
+        this.commitInfo = commit!.sha
+                + '~'
+                + commit!.message
+                + ' Branches :'
+                + commit!.branchIds!.map(x => this.branchById.get(x)!.name).join(', \n')
+                + ' '
+                + commit!.branchIds!.length
+                + '/'
+                + this.branchById.size
+      }
+
+    })
+    g.selectAll('g.node').on('mouseout', () => {
+      this.commitInfo = undefined;
+
+    })
+    console.log('Done rendering base')
+  }
+
+  private drawBaseGraph(graph: Graph, g: d3.Selection<SVGGElement, unknown, HTMLElement, any>) {
     this.nodes?.forEach(x => this.setCommit(graph, x))
     this.links?.forEach(x => this.setEdge(graph, x))
-    this.nodes?.forEach(x => {
-      if (this.branchesByHeadCommitId.has(x.id!!)) {
-        this.setBranchLabel(graph, this.branchesByHeadCommitId.get(x.id!!)!!, g);
-      }
-    })
+    const renderer = render();
     renderer(g, graph);
+  }
 
-    svg.attr("width", "100%")
-    svg.attr("height", "90vh")
-    svg.call(d3.zoom<any, any>()
-            .scaleExtent([0.1, 8])
-            .on('zoom', function ({transform}) {
-              g.attr('transform', transform);
-            }));
-    g.selectAll("g.node").on('mouseover', (event, commitNodeId) => {
-      if ((commitNodeId as string).indexOf(this.COMMIT_NODE_ID_PREFIX) === 0) {
-        const commitId = (commitNodeId as string).replace(this.COMMIT_NODE_ID_PREFIX, "");
-        const commit = this.commits.get(+commitId);
-        this.commitInfo = commit!!.sha + "~" + commit!!.message
+  private setBranchLabel() {
+    const g = d3.select('#git-graph-group') as d3.Selection<SVGGElement, unknown, HTMLElement, any>;
+    const branchLabelG = g.select('.output')
+            .append('g')
+            .attr('class', 'branch-label') as d3.Selection<SVGGElement, unknown, HTMLElement, any>;
+    this.nodes?.forEach(x => {
+      if (this.branchesByHeadCommitId.has(x.id!)) {
+        const gitBranches = this.branchesByHeadCommitId.get(x.id!)!;
+        gitBranches.forEach(branch => {
+          const commitNode = g.select('#' + this.COMMIT_NODE_ID_PREFIX + branch.headCommitId);
+          const commitNodeBBox = (commitNode.node()! as SVGSVGElement).getBoundingClientRect();
+          const branchLabelPadding = 10;
+          const branchLabelHeight = 20;
+          const branchLabel = branchLabelG.append('g')
+                  .attr('class', 'branch-label');
+
+          const branchLabelContainer = branchLabel.append('rect')
+                  .attr('class', 'branch-label-container')
+                  .attr('height', branchLabelHeight)
+                  .attr('rx', 2)
+                  .attr('ry', 2)
+                  .style('fill', 'beige')
+                  .style('stroke', 'brown')
+
+          const label = branchLabel.append('g')
+                  .attr('class', 'label')
+                  .append('text')
+                  .append('tspan')
+                  .attr('space', 'preserve')
+                  .attr('dy', '1em')
+                  .attr('x', branchLabelPadding)
+                  .text(branch.name + '')
+
+          // Calculate width of the label text
+          const textWidth = label.node()?.getComputedTextLength() || 0;
+          // Set width of branch label container
+          branchLabelContainer.attr('width', textWidth + branchLabelPadding * 2);
+          // Position the branchLabel
+          const x = commitNodeBBox.x
+                  - branchLabel.node()!.getBoundingClientRect().x
+                  - textWidth
+                  - 30;
+          const y = commitNodeBBox.y
+                  - branchLabel.node()!.getBoundingClientRect().y
+                  + branchLabelHeight
+                  / 2;
+          branchLabel.attr('transform', `translate(${x},${y})`);
+        });
       }
-
     })
   }
 
   private setCommit(graph: graphlib.Graph, gitCommit: GitCommitDto) {
     let color = 'none';
     if (gitCommit.branchIds?.length == 1) {
-      const sha = this.commits.get(this.branchById.get(gitCommit.branchIds[ 0 ])!!.headCommitId!!)!!.sha!!;
+      const sha = this.commits.get(this.branchById.get(gitCommit.branchIds[ 0 ])!.headCommitId!)!.sha!;
       color = this.getColorFromShaString(sha);
     }
     graph.setNode(this.COMMIT_NODE_ID_PREFIX + gitCommit.id, {
+      id: this.COMMIT_NODE_ID_PREFIX + gitCommit.id,
+      class: 'commit-node',
+      label: gitCommit.id + '',
       radius: 5,
       shape: 'circle',
       style: `stroke: black; fill:${color}; stroke-width: 1px;`
     });
-  }
-
-  private setBranchLabel(graph: graphlib.Graph, gitBranches: GitBranchDto[], g: d3.Selection<SVGGElement, unknown, HTMLElement, any>) {
-    gitBranches.forEach(branch => {
-      graph.setNode(this.BRANCH_NODE_ID_PREFIX + branch.id, {
-        label: branch.name,
-        height: 5,
-        shape: 'rect',
-        style: 'stroke: brown; fill:beige; stroke-width: 1px;'
-      });
-      graph.setEdge(this.COMMIT_NODE_ID_PREFIX + branch.headCommitId + '',
-              this.BRANCH_NODE_ID_PREFIX + branch.id,
-              {
-                lineInterpolate: 'basis',
-                style: 'stroke: grey; fill:none; stroke-width: 1px; stroke-dasharray="5,5" d="M5 20 l215 0',
-              });
-    });
-
   }
 
   private setEdge(graph: graphlib.Graph, gitCommitRelation: GitCommitRelationshipDto) {
