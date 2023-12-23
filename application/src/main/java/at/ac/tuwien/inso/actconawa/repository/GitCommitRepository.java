c602862cfceb75091c18a452366305a149565b73
Thomas Moerbauer <tmoer93@gmail.com>
2023-12-23 17:41:26 +0100

    :art: Format find ancestor query

--- application/src/main/java/at/ac/tuwien/inso/actconawa/repository/GitCommitRepository.java
+++ application/src/main/java/at/ac/tuwien/inso/actconawa/repository/GitCommitRepository.java
@@ -10,17 +10,29 @@ import java.util.UUID;
 
 public interface GitCommitRepository extends JpaRepository<GitCommit, UUID> {
 
-    @Query(value = "WITH RECURSIVE linked_entries(id, parent_id, child_id, stop, depth) AS (\n"
-            + "  SELECT icr.id, icr.parent_id  , icr.child_id, (select count(c.child_id) from commit_relationship c where c.child_id = icr.child_id) > 1, 0\n"
-            + "  FROM commit_relationship icr\n"
-            + "  WHERE icr.child_id = :childId\n"
-            + "  UNION\n"
-            + "  SELECT ou.id, ou.parent_id, ou.child_id, (select count(c.child_id) from commit_relationship c where c.child_id = ou.child_id) > 1, depth + 1\n"
-            + "  FROM linked_entries\n"
-            + "  INNER JOIN commit_relationship AS ou ON linked_entries.parent_id = ou.child_id\n"
-            + "  WHERE stop is false and depth < :maxDepth"
-            + ")\n"
-            + "SELECT distinct comm.* FROM linked_entries le join commit comm on comm.id = child_id order by comm.max_distance_from_root desc ;", nativeQuery = true)
+    @Query(value = """
+                   WITH RECURSIVE linked_entries(id, parent_id, child_id, stop, depth) AS (
+                     SELECT icr.id,
+                            icr.parent_id ,
+                            icr.child_id,
+                            (SELECT count(c.child_id) FROM commit_relationship c WHERE c.child_id = icr.child_id) > 1,
+                            0
+                     FROM commit_relationship icr
+                     WHERE icr.child_id = :childId
+                     UNION
+                     SELECT ou.id,
+                            ou.parent_id,
+                            ou.child_id,
+                            (SELECT count(c.child_id) FROM commit_relationship c WHERE c.child_id = ou.child_id) > 1,
+                            depth + 1
+                     FROM linked_entries
+                     INNER JOIN commit_relationship AS ou ON linked_entries.parent_id = ou.child_id
+                     WHERE stop is false AND depth < :maxDepth
+                   )
+                   SELECT distinct comm.*
+                   FROM linked_entries le JOIN commit comm ON comm.id = child_id
+                   ORDER BY comm.max_distance_from_root DESC ;
+            """, nativeQuery = true)
     List<GitCommit> findAncestors(@Param("childId") UUID childId, @Param("maxDepth") int maxDepth);
 
     @Query("select r.child from GitCommitRelationship r where r.parent is null ")
