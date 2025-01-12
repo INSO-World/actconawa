4acf134cfdca276973a9677f1e3cbe0b2c2e1dd8
Thomas Moerbauer <tmoer93@gmail.com>
2025-01-12 17:33:17 +0100

    :sparkles: reference branch settings

--- frontend/src/app/modules/setting/repository-configuration/repository-configuration.component.ts
+++ frontend/src/app/modules/setting/repository-configuration/repository-configuration.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GitService } from "../../../services/git.service";
+import { GitBranchDto } from "../../../../api";
+import { SettingService } from "../../../services/setting.service";
 
 @Component({
   selector: 'app-repository-configuration',
@@ -8,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class RepositoryConfigurationComponent {
 
+  protected gitService = inject(GitService)
+
+  protected settingService = inject(SettingService)
+
+  protected branches: GitBranchDto[] = []
+
+  async ngOnInit() {
+    this.branches = await this.gitService.getBranches();
+  }
+
+  onSelectReferenceBranch(event: Event) {
+    this.settingService.setReferenceBranchName((event.target as HTMLSelectElement).value)
+  }
 }
--- frontend/src/app/modules/setting/setting.module.ts
+++ frontend/src/app/modules/setting/setting.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { SettingRoutingModule } from './setting-routing.module';
 import { RepositoryConfigurationComponent } from './repository-configuration/repository-configuration.component';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatCard } from "@angular/material/card";
+import { MatInput } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { RepositoryConfigurationComponent } from './repository-configuration/rep
   ],
   imports: [
     CommonModule,
-    SettingRoutingModule
+    SettingRoutingModule,
+    MatLabel,
+    MatFormField,
+    MatCard,
+    MatInput
   ]
 })
 export class SettingModule { }
