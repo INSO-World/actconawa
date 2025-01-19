a7d77b87a03319d642d94c8a858bcacdee2428f9
Thomas Moerbauer <tmoer93@gmail.com>
2025-01-19 01:20:20 +0100

    :bug: Fix compositekeymap to respect order of key (no symmetry)

--- frontend/src/app/utils/CompositeKeyMap.ts
+++ frontend/src/app/utils/CompositeKeyMap.ts
@@ -3,13 +3,9 @@ export class CompositeKeyMap<K, V> {
   private innerMap: Map<K, Map<K, V>> = new Map<K, Map<K, V>>();
 
   get(keyA: K, keyB: K): V | undefined {
-    return this.innerMap.get(keyA)?.get(keyB) || this.innerMap.get(keyB)?.get(keyA);
+    return this.innerMap.get(keyA)?.get(keyB);
   }
 
-  /**
-   * Super inperformant. only use for debugging
-   * @param key
-   */
   getAllOfKey(key: K): V[] {
     const result = new Set<V>
     const firstLevel = this.innerMap.get(key);
@@ -18,31 +14,20 @@ export class CompositeKeyMap<K, V> {
         result.add(values);
       }
     }
-    for (const currentKey of this.innerMap.keys()) {
-      if (key === currentKey) {
-        continue;
-      }
-      const temp = this.innerMap.get(currentKey)?.get(key);
-      if (temp) {
-        result.add(temp);
-      }
-    }
     return Array.from(result);
   }
 
   has(keyA: K, keyB: K): boolean {
-    return this.innerMap.get(keyA)?.has(keyB) || this.innerMap.get(keyB)?.has(keyA) || false;
+    return this.innerMap.get(keyA)?.has(keyB) || false;
   }
 
   set(keyA: K, keyB: K, value: V): this {
-    if (!this.innerMap.has(keyA) && !this.innerMap.has(keyB)) {
+    if (!this.innerMap.has(keyA)) {
       const innerInnerMap = new Map<K, V>;
       innerInnerMap.set(keyB, value);
       this.innerMap.set(keyA, innerInnerMap)
     } else if (this.innerMap.has(keyA)) {
       this.innerMap.get(keyA)?.set(keyB, value);
-    } else if (this.innerMap.has(keyB)) {
-      this.innerMap.get(keyB)?.set(keyA, value);
     }
     return this;
   }
